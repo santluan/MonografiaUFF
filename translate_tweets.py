8a9e1738ccea0dd9ee8e6ea339811641b61ac321
Luan Santos <santosluan@id.uff.br>
2022-10-15 18:26:42 -0300

    Update translate_tweets.py

--- translate_tweets.py
+++ translate_tweets.py
@@ -2,18 +2,14 @@
 # author: Luan Santos
 # 15-10-2022
 
-import re
 import time
 import pandas as pd
 from googletrans import Translator
 translator = Translator()
 
 # read tweets csv
-path = r'D:/Desktop/DataScience/Python/monografia/'
+path = r'../monografia/'
 df = pd.read_csv(path + 'folhamercado_tweets.csv')
-
-# remove url from tweets
-df['clean_tweet'] = df['tweet'].apply(lambda x: re.sub("http\S+", "", x))
 
 # function to translate tweets with interval of 0.1 sec
 def translate(x):
