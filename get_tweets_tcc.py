f6b230343b66241ddb594e680514f662a3ad0f64
Luan Santos <santosluan@id.uff.br>
2022-10-15 17:38:47 -0300

    Add files via upload

---
+++ get_tweets_tcc.py
@@ -0,0 +1,28 @@
+# Code to extract tweets from @folha_mercado using snscrape
+# author: Luan Santos
+
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+# my path
+my_path = "D:/Desktop/DataScience/Python/monografia/Data/"
+
+# query to search tweets
+query = "(from:folha_mercado) until:2021-12-31 since:2011-01-01"
+tweets = []
+limit = 100000
+
+# loop for to extract tweets
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date, tweet.user.username, tweet.content])
+
+# storing tweets on dataframe
+df = pd.DataFrame(tweets, columns=['date', 'user', 'tweet'])
+df['Date'] = df['Date'].apply(lambda a: pd.to_datetime(a).date())
+
+# save in csv file
+filename = 'folhamercado_tweets.csv'
+df.to_csv(my_path + filename, index=False, encoding='utf-8-sig')
