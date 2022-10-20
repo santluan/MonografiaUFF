eb0d27d8235419f0d2a69f8f7b67a5ae868796d3
Luan Santos <santosluan@id.uff.br>
2022-10-20 20:17:21 -0300

    Update vader_implementation.py

--- vader dict/vader_implementation.py
+++ vader dict/vader_implementation.py
@@ -10,7 +10,7 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 sid = SentimentIntensityAnalyzer()
 
 # read database
-path = r'D:/Desktop/DataScience/Python/monografia/'
+path = r'../monografia/'
 df = pd.read_csv(path + 'folhamercado_tweets.csv', usecols=['date', 'tweetTranslated'])
 
 # transform index to date format
@@ -18,9 +18,10 @@ df.index = pd.to_datetime(df['date'])
 
 # columns to scores, compound, comp_scores and sentiment index
 df['scores'] = df.tweetTranslated.apply(lambda x: sid.polarity_scores(x))
-df['compound'] = df['scores'].apply(lambda score_dict: score_dict['compound'])
-df['comp_score'] = df['compound'].apply(lambda c: 'pos' if c >= 0 else 'neg')
-df['sentiment_index'] = df['scores'].apply(lambda score: score['pos'] - score['neg'])
+df['pos_sentiment'] = df['scores'].apply(lambda score: score['pos'])
+df['neg_sentiment'] = df['scores'].apply(lambda score: score['neg'])
+df['compound'] = df['scores'].apply(lambda score: score['compound'])
+df['sentiment_index'] = df.pos_sentiment - df.neg_sentiment
 
 df.to_csv(path + 'vader/vader_index.csv')
 
@@ -39,21 +40,3 @@ vaderIndex_month.to_csv(path + "vader/vaderIndex_month.csv")
 # quarter index
 vaderIndex_quarter = df.groupby(pd.Grouper(freq="QS")).mean()
 vaderIndex_quarter.to_csv(path + "vader/vaderIndex_quarter.csv")
-
-# function to plot index
-def plot_index(data, period):
-    plt.plot('compound', data=data, label='Compound')
-    plt.plot('sentiment_index', data=data, label='Sentiment Index')
-    plt.title('Compound x Sentiment Index by ' + period)
-    plt.legend()
-    plt.show()
-
-
-plot_index(vaderIndex_diary, 'day')
-plt.savefig(path + 'vader/sentimentIndex_diary.png')
-
-plot_index(vaderIndex_month, 'month')
-plt.savefig(path + 'vader/sentimentIndex_month.png')
-
-plot_index(vaderIndex_quarter, 'quarter')
-plt.savefig(path + 'vader/sentimentIndex_quarter.png')
