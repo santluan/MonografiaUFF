368ab595fb1c1babe45d8fe98b35ead7a027be98
Luan Santos <santosluan@id.uff.br>
2022-12-07 21:59:44 -0300

    Update vader_plots.py

--- vader dict/vader_plots.py
+++ vader dict/vader_plots.py
@@ -1,33 +1,16 @@
 ########## PLOTS ##############
+# Luan Santos
+# 07-12-2022
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-path = 'D:/Desktop/DataScience/Python/monografia/vader/'
-vaderIndex_diary = pd.read_csv(path + 'vaderIndex_diary.csv')
-vaderIndex_diary.index = pd.to_datetime(vaderIndex_diary['date'])
+path = '../monografia/vader/'
 
+# load data
 vaderIndex_month = pd.read_csv(path + 'vaderIndex_month.csv')
-vaderIndex_month.index = pd.to_datetime(vaderIndex_diary['date'])
-
-vaderIndex_quarter = pd.read_csv(path + 'vaderIndex_quarter.csv')
-vaderIndex_quarter.index = pd.to_datetime(vaderIndex_diary['date'])
-
-df = pd.read_csv(path + 'vader_index.csv')
-
-# plot compound index
-plt.plot(df.compound)
-plt.title('compound index for folha_mercado tweets - using all tweets')
-plt.draw()
-plt.show()
-plt.savefig(path + 'vader/compound_index.png')
-
-# plot sentiment index
-plt.plot(df.sentiment_index)
-plt.title('sentiment index for folha_mercado tweets - using all tweets')
-plt.draw()
-plt.show()
-plt.savefig(path + 'vader/sentiment_index.png')
+vaderIndex_month.index = pd.to_datetime(vaderIndex_month['date'])
 
 # function to plot index
 def plot_index(data, period):
@@ -37,12 +20,27 @@ def plot_index(data, period):
     plt.legend()
     plt.show()
 
-
-plot_index(vaderIndex_diary, 'day')
-plt.savefig(path + 'vader/sentimentIndex_diary.png')
-
 plot_index(vaderIndex_month, 'month')
 plt.savefig(path + 'vader/sentimentIndex_month.png')
 
-plot_index(vaderIndex_quarter, 'quarter')
-plt.savefig(path + 'vader/sentimentIndex_quarter.png')
+# graph to monograph
+# monthly index
+plt.style.use('seaborn-whitegrid')
+#plt.figure(figsize=[7.5, 5.5])
+plt.plot('sentiment_index', data=vaderIndex_month, label='Índice de Sentimentos VADER', color='#FF0000')
+plt.legend(bbox_to_anchor=(0.5,-0.15), loc='lower center')
+plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=True)
+plt.tick_params(axis='y', which='both', right=False, left=False, labelleft=True)
+for pos in ['right', 'top', 'bottom', 'left']:
+    plt.gca().spines[pos].set_visible(False)
+plt.show()
+plt.savefig(path + 'isv.pdf')
+
+# density plot
+sns.set_style('white')
+isv1 = sns.displot(data=vaderIndex_month, x='sentiment_index', kde=True, color='red')
+isv1.set(xlabel='ISV', ylabel="Densidade")
+for pos in ['right', 'top', 'bottom', 'left']:
+    plt.gca().spines[pos].set_visible(False)
+plt.show()
+plt.savefig(path + 'isv_densityplot.pdf')
