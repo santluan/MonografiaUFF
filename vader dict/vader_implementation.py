8c18be490153e3a19dcdbce895eb21e3694527ce
Luan Santos <santosluan@id.uff.br>
2022-12-07 21:44:32 -0300

    Update vader_implementation.py

--- vader dict/vader_implementation.py
+++ vader dict/vader_implementation.py
@@ -3,14 +3,13 @@
 # 17-10-2022
 
 import pandas as pd
-import matplotlib.pyplot as plt
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # Vader Sentiment Model
 sid = SentimentIntensityAnalyzer()
 
 # read database
-path = r'../monografia/'
+path = r'D:/Desktop/DataScience/Python/monografia/'
 df = pd.read_csv(path + 'folhamercado_tweets.csv', usecols=['date', 'tweetTranslated'])
 
 # transform index to date format
@@ -25,18 +24,6 @@ df['sentiment_index'] = df.pos_sentiment - df.neg_sentiment
 
 df.to_csv(path + 'vader/vader_index.csv')
 
-# descriptive stats from compound and sentiment index
-df.compound.describe()
-df.sentiment_index.describe()
-
 # diary index
-vaderIndex_diary = df.groupby(pd.Grouper(freq="D")).mean()
+vaderIndex_diary = df.groupby(pd.Grouper(freq="D")).mean(numeric_only=True)
 vaderIndex_diary.to_csv(path + "vader/vaderIndex_diary.csv")
-
-# month index
-vaderIndex_month = df.groupby(pd.Grouper(freq="M")).mean()
-vaderIndex_month.to_csv(path + "vader/vaderIndex_month.csv")
-
-# quarter index
-vaderIndex_quarter = df.groupby(pd.Grouper(freq="QS")).mean()
-vaderIndex_quarter.to_csv(path + "vader/vaderIndex_quarter.csv")
